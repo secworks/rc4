9983e5fd29c28660459ea34ba5a2384a8a32cc78
Joachim Strömbergson <joachim@secworks.se>
2014-01-28 22:11:49 +0100

    Removed trailing whitespace.

--- src/rtl/rc4.v
+++ src/rtl/rc4.v
@@ -9,30 +9,30 @@
 //
 // Copyright (c) 2013 Secworks Sweden AB
 // All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or 
-// without modification, are permitted provided that the following 
-// conditions are met: 
-// 
-// 1. Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer. 
-// 
-// 2. Redistributions in binary form must reproduce the above copyright 
-//    notice, this list of conditions and the following disclaimer in 
-//    the documentation and/or other materials provided with the 
-//    distribution. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
-// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
 // BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //======================================================================
@@ -40,21 +40,21 @@
 module rc4(
            input wire          clk,
            input wire          reset_n,
-           
+
            input wire          init,
            input wire          next,
            input wire          rfc4345_mode,
-           
+
            input wire [4 : 0]  key_addr,
            input wire [7 : 0]  key_data,
            input wire          key_write,
            input wire          key_size,
-           
+
            output wire [7 : 0] keystream_data,
            output wire         keystream_valid
           );
-  
-  
+
+
   //----------------------------------------------------------------
   // Internal constant and parameter definitions.
   //----------------------------------------------------------------
@@ -65,7 +65,7 @@ module rc4(
   parameter CTRL_SKIP = 2;
   parameter CTRL_NEXT = 3;
 
-  
+
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
@@ -84,19 +84,19 @@ module rc4(
   reg         rc4_ctr_rst;
   reg         rc4_ctr_inc;
   reg         rc4_ctr_we;
-  
+
   reg [1 : 0] rc4_ctrl_reg;
   reg [1 : 0] rc4_ctrl_new;
   reg         rc4_ctrl_we;
 
-  
+
   //----------------------------------------------------------------
   // Wires.
   //----------------------------------------------------------------
   wire [7 : 0] idata;
   wire [7 : 0] jdata;
   wire [7 : 0] kdata;
-  reg [7 : 0] kp_new;
+  reg [7 : 0]  kp_new;
 
   reg smem_swap;
   reg smem_init;
@@ -108,11 +108,11 @@ module rc4(
   reg update_state;
   reg update_regs;
   reg ksa;
-  
+
   reg [7 : 0] tmp_keystream_data;
   reg skip_data;
 
-  
+
   //----------------------------------------------------------------
   // Module instantiantions.
   //----------------------------------------------------------------
@@ -122,7 +122,7 @@ module rc4(
 
                      .init(smem_init),
                      .swap(smem_swap),
-                     
+
                      .i_read_addr(ip_new),
                      .i_read_data(idata),
 
@@ -132,31 +132,31 @@ module rc4(
                      .k_read_addr(kp_new),
                      .k_read_data(kdata)
                     );
-  
+
 
   rc4_key_mem kmem(
                    .clk(clk),
                    .reset_n(reset_n),
-                   
+
                    .init(kmem_init),
 
                    .key_write_addr(key_addr),
                    .key_write_data(key_data),
                    .key_write(key_write),
                    .key_size(key_size),
-                   
+
                    .key_read_addr(ip_reg[4 : 0]),
                    .key_read_data(kmem_data)
                   );
 
-  
+
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
   assign keystream_valid = keystream_valid_reg;
   assign keystream_data = tmp_keystream_data;
-  
-  
+
+
   //----------------------------------------------------------------
   // reg_update
   // Update functionality for all registers in the core.
@@ -190,7 +190,7 @@ module rc4(
             begin
               keystream_valid_reg <= keystream_valid_new;
             end
-          
+
           if (rc4_ctrl_we)
             begin
               rc4_ctrl_reg <= rc4_ctrl_new;
@@ -209,7 +209,7 @@ module rc4(
     begin : output_logic
       keystream_valid_new = 0;
       keystream_valid_we = 0;
-      
+
       if (skip_data)
         begin
           tmp_keystream_data  = 8'h00;
@@ -223,8 +223,8 @@ module rc4(
           keystream_valid_we  = 1;
         end
     end
-  
-  
+
+
   //----------------------------------------------------------------
   // rc4_logic
   //
@@ -245,7 +245,7 @@ module rc4(
           jp_new      = 8'h00;
           update_regs = 1;
         end
-      
+
       if (update_state)
         begin
           smem_swap   = 1;
@@ -264,7 +264,7 @@ module rc4(
           kp_new = idata + jdata;
         end
     end // rc4_logic
-  
+
 
   //----------------------------------------------------------------
   // rc4_ctr_logic
@@ -290,7 +290,7 @@ module rc4(
         end
     end
 
-  
+
   //----------------------------------------------------------------
   // rc4_ctrl_fsm
   //
@@ -309,10 +309,10 @@ module rc4(
       rc4_ctr_inc   = 0;
 
       skip_data     = 1;
-      
+
       rc4_ctrl_new  = CTRL_IDLE;
       rc4_ctrl_we   = 0;
-      
+
       case (rc4_ctrl_reg)
         CTRL_IDLE:
           begin
@@ -360,7 +360,7 @@ module rc4(
                 rc4_ctrl_we  = 1;
               end
           end
-        
+
         CTRL_NEXT:
           begin
             skip_data = 0;
